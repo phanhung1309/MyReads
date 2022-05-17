772ebdb64f4a0a95bc8bd2b0f97fb93c043f19d3
Hung Phan <phanhung1309@gmail.com>
2022-05-17 23:27:03 +0700

    Add react-router-dom and prettier

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>My Reads</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
-}
+};
 
 export default App;
