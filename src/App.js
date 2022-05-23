833d07ae5e97d4620483b48964f3242e552a0aa3
Hung Phan <phanhung1309@gmail.com>
2022-05-23 23:34:22 +0700

    Add book details router

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
 import NotFound from "./pages/NotFound";
+import BookDetails from "./pages/BookDetails";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/:bookId" element={<BookDetails />} />
       <Route path="/search" element={<Search />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
